2a856e52e215d0a5b39cfed910aa378faeddeb0e
Lukas Eder <lukas.eder@gmail.com>
2013-10-01 11:34:48 +0200

    Added test using asterisk

---
+++ sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest5.java
@@ -0,0 +1,45 @@
+package gudusoft.sql2jooq.sakila.tests;
+
+import static org.jooq.impl.DSL.*;
+import static org.junit.Assert.*;
+import static gudusoft.sakila.Tables.*;
+
+import java.sql.*;
+
+import org.jooq.*;
+import org.jooq.impl.*;
+import org.junit.*;
+
+import gudusoft.sakila.tables.*;
+import gudusoft.sql2jooq.sakila.MySQLTest;
+
+/**
+ * @author Lukas Eder
+ */
+public class SakilaTest5 extends MySQLTest
+{
+
+	@Test
+	public void test() throws Exception 
+	{
+		String sql = "select * from actor;";
+		
+		if (sql.toLowerCase().startsWith("select")) 
+		{
+			assertSame(ctx.fetch(sql), generatedSQL(conn));
+		}
+		else 
+		{
+			throw new UnsupportedOperationException("DML is not yet supported");
+		}
+	}
+	
+	private static Result generatedSQL( Connection conn )
+	{
+		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+Result result = create.select(  )
+	.from( Actor.ACTOR ).fetch( );
+
+		return result;
+	}
+}
