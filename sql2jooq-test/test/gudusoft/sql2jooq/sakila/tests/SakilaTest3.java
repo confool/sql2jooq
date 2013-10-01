4365472d34b8ea480cfbb4c98f92861f0caa9101
Lukas Eder <lukas.eder@gmail.com>
2013-10-01 11:20:37 +0200

    [#4] Added generated JUnit tests

---
+++ sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest3.java
@@ -0,0 +1,46 @@
+package gudusoft.sql2jooq.sakila.tests;
+
+import static org.jooq.impl.DSL.*;
+import static org.junit.Assert.*;
+import static gudusoft.sakila.Tables.*;
+
+import java.sql.*;
+
+import org.jooq.*;
+import org.jooq.impl.*;
+import org.junit.*;
+
+import gudusoft.sakila.tables.*;
+import gudusoft.sql2jooq.sakila.MySQLTest;
+
+/**
+ * @author Lukas Eder
+ */
+public class SakilaTest3 extends MySQLTest
+{
+
+	@Test
+	public void test() throws Exception 
+	{
+		String sql = "select first_name, last_name from actor where actor_id = 1;";
+		
+		if (sql.toLowerCase().startsWith("select")) 
+		{
+			assertSame(ctx.fetch(sql), generatedSQL(conn));
+		}
+		else 
+		{
+			throw new UnsupportedOperationException("DML is not yet supported");
+		}
+	}
+	
+	private static Result generatedSQL( Connection conn )
+	{
+		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+Result result = create.select( ((Field)Actor.ACTOR.FIRST_NAME), ((Field)Actor.ACTOR.LAST_NAME) )
+	.from( Actor.ACTOR )
+	.where( ((Field)Actor.ACTOR.ACTOR_ID).equal( 1 ) ).fetch( );
+
+		return result;
+	}
+}
