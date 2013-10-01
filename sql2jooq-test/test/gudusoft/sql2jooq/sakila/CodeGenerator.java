8dac75fe25f73496787cf74ce7babf20e695d426
Lukas Eder <lukas.eder@gmail.com>
2013-10-01 11:20:27 +0200

    [#4] Add JUnit test suite generator for queries against the Sakila database

---
+++ sql2jooq-test/test/gudusoft/sql2jooq/sakila/CodeGenerator.java
@@ -0,0 +1,62 @@
+
+package gudusoft.sql2jooq.sakila;
+
+import gudusoft.gsqlparser.EDbVendor;
+import gudusoft.gsqlparser.sql2jooq.jooqConverter;
+import gudusoft.sql2jooq.util.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * @author Lukas Eder
+ */
+public class CodeGenerator
+{
+
+    public static void main( String[] args ) throws IOException
+    {
+
+        // jOOQ's MockFileDatabase can be used for unit testing
+        // It allows for matching queries with expected results
+        String[] queries = FileUtil
+                .read(new File("./test/gudusoft/sql2jooq/sakila/queries.txt"))
+                .split(";\\s+");
+
+        int i = 0;
+        for (final String query : queries)
+        {
+            if (query == null || query.length() == 0)
+                continue;
+
+            i++;
+
+            try {
+                System.out.println("Generating code for : " + query);
+
+                jooqConverter convert = new jooqConverter( EDbVendor.dbvmysql, query );
+                final String result = convert.getConvertResult( );
+                final String id = "" + i;
+
+                File templateFile = new File( "./xml/guestbook/SakilaTest.template.java" );
+                File saveFile = new File( "./test/gudusoft/sql2jooq/sakila/tests/SakilaTest" + id + ".java" );
+
+                Map<String, String> replacements = new HashMap<String, String>();
+                replacements.put(Pattern.quote( "${ID}" ), id);
+                replacements.put(Pattern.quote( "${DSL}" ), result);
+                replacements.put(Pattern.quote( "${SQL}" ), query.replace("\"", "\\\""));
+
+                FileUtil.replaceFile( templateFile,
+                        ".*",
+                        replacements,
+                        saveFile );
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ sql2jooq-test/xml/guestbook/SakilaTest.template.java
@@ -0,0 +1,42 @@
+package gudusoft.sql2jooq.sakila.tests;
+
+import static org.jooq.impl.DSL.*;
+import static org.junit.Assert.*;
+import static gudusoft.sakila.Tables.*;
+
+import java.sql.*;
+
+import org.jooq.*;
+import org.jooq.impl.*;
+import org.junit.*;
+
+import gudusoft.sakila.tables.*;
+import gudusoft.sql2jooq.sakila.MySQLTest;
+
+/**
+ * @author Lukas Eder
+ */
+public class SakilaTest${ID} extends MySQLTest
+{
+
+	@Test
+	public void test() throws Exception 
+	{
+		String sql = "${SQL}";
+		
+		if (sql.toLowerCase().startsWith("select")) 
+		{
+			assertSame(ctx.fetch(sql), generatedSQL(conn));
+		}
+		else 
+		{
+			throw new UnsupportedOperationException("DML is not yet supported");
+		}
+	}
+	
+	private static Result generatedSQL( Connection conn )
+	{
+		${DSL}
+		return result;
+	}
+}
