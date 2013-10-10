3dba7237e05ef70da1bef891ed47527b9ad06974
cnfree <cnfree2000@hotmail.com>
2013-10-11 00:34:49 +0800

    Issue #10 : update the guest book test case.

--- sql2jooq-test/test/gudusoft/sql2jooq/guestbook/testGuestBook.java
+++ sql2jooq-test/test/gudusoft/sql2jooq/guestbook/testGuestBook.java
@@ -7,11 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.jooq.DSLContext;
-import org.jooq.Field;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 
 public class testGuestBook
@@ -71,11 +67,11 @@ public class testGuestBook
 		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
 Posts a = Posts.POSTS.as("a");
 Mails b = Mails.MAILS.as("b");
-Result<org.jooq.Record4<java.lang.Long, java.lang.String, java.lang.String, java.lang.String>> result = create.select( b.ID, a.TITLE, a.BODY, b.ADDRESS )
+Result<Record4<Long, String, String, String>> result = create.select( b.ID, a.TITLE, a.BODY, b.ADDRESS )
 	.from( a, b )
-	.where( a.BODY.equal( java.lang.String.valueOf( "Hello World" ) ).and( a.TIMESTAMP.equal( java.sql.Timestamp.valueOf( "2003-10-01 00:24:08" ) ) ).and( a.ID.equal( java.lang.Long.valueOf( 1 ) ) ) )
+	.where( a.BODY.equal( DSL.inline( String.valueOf( "Hello World" ) ) ).and( a.TIMESTAMP.equal( DSL.inline( java.sql.Timestamp.valueOf( "2003-10-01 00:24:08" ) ) ) ).and( a.ID.equal( DSL.inline( Long.valueOf( 1 ) ) ) ) )
 	.groupBy( a.BODY, a.ID )
-	.having( DSL.count(  ).greaterThan( 0 ) )
+	.having( DSL.count(  ).greaterThan( DSL.inline( 0 ) ) )
 	.orderBy( a.ID ).fetch( );
 
 		return result;
