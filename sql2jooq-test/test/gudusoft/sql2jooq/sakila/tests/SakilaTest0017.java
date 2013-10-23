b40850cf0a886eab63f12f1dbff9409f9631fe05
Lukas Eder <lukas.eder@gmail.com>
2013-10-23 15:58:08 +0200

    Added test cases for NULL predicate

---
+++ sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest0017.java
@@ -0,0 +1,47 @@
+package gudusoft.sql2jooq.sakila.tests;
+
+import static org.jooq.impl.DSL.*;
+import static org.junit.Assert.*;
+import static gudusoft.sakila.Tables.*;
+
+import java.sql.*;
+
+import org.jooq.*;
+import org.jooq.impl.*;
+import org.junit.*;
+
+import gudusoft.sakila.tables.*;
+import gudusoft.sql2jooq.sakila.MySQLTest;
+
+/**
+ * @author Lukas Eder
+ */
+public class SakilaTest0017 extends MySQLTest
+{
+
+	@Test
+	public void test() throws Exception 
+	{
+		String sql = "select count(*) c from actor where first_name is null";
+		
+		if (sql.toLowerCase().startsWith("select")) 
+		{
+			assertSame(ctx.fetch(sql), generatedSQL(conn));
+		}
+		else 
+		{
+			throw new UnsupportedOperationException("DML is not yet supported");
+		}
+	}
+	
+	private static Result generatedSQL( Connection conn )
+	{
+		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+
+Result result = create.select( DSL.count(  ).as("c") )
+	.from( Actor.ACTOR )
+	.where( ((Field)Actor.ACTOR.FIRST_NAME).isNull(  ) ).fetch( );
+
+		return result;
+	}
+}
