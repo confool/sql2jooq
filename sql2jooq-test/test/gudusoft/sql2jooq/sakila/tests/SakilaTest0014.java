b0e197cfc89c75f1ac77483a1f21e4875c602f85
Lukas Eder <lukas.eder@gmail.com>
2013-10-23 15:54:25 +0200

    Added test cases for BETWEEN predicate

---
+++ sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest0014.java
@@ -0,0 +1,47 @@
+package gudusoft.sql2jooq.sakila.tests;
+
+import static org.jooq.impl.DSL.*;
+import static org.junit.Assert.*;
+import static gudusoft.sakila.Tables.*;
+
+import java.sql.*;
+
+import org.jooq.*;
+import org.jooq.impl.*;
+import org.junit.*;
+
+import gudusoft.sakila.tables.*;
+import gudusoft.sql2jooq.sakila.MySQLTest;
+
+/**
+ * @author Lukas Eder
+ */
+public class SakilaTest0014 extends MySQLTest
+{
+
+	@Test
+	public void test() throws Exception 
+	{
+		String sql = "select 1 from dual where 2 not between 3 and 4;";
+		
+		if (sql.toLowerCase().startsWith("select")) 
+		{
+			assertSame(ctx.fetch(sql), generatedSQL(conn));
+		}
+		else 
+		{
+			throw new UnsupportedOperationException("DML is not yet supported");
+		}
+	}
+	
+	private static Result generatedSQL( Connection conn )
+	{
+		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+
+Result result = create.select( DSL.inline( 1 ) )
+	.from( DSL.dual() )
+	.where( DSL.inline( 2 ).notBetween( DSL.inline( 3 ) ).and( DSL.inline( 4 ) ) ).fetch( );
+
+		return result;
+	}
+}
