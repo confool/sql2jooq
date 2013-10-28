f8d24d586292ed53eacf92436b03cc859e7e1c5f
Lukas Eder <lukas.eder@gmail.com>
2013-10-28 10:52:50 +0100

    [#29] Added failing integration test for WITH ROLLUP

--- sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest0047.java
+++ sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest0047.java
@@ -16,13 +16,13 @@ import gudusoft.sql2jooq.sakila.MySQLTest;
 /**
  * @author Lukas Eder
  */
-public class SakilaTest0046 extends MySQLTest
+public class SakilaTest0047 extends MySQLTest
 {
 
 	@Test
 	public void test() throws Exception 
 	{
-		String sql = "select first_name, last_name, count(*) from actor group by first_name, last_name having count(*) > 1";
+		String sql = "select first_name, last_name, count(*) from actor group by first_name, last_name with rollup;";
 		
 		if (sql.toLowerCase().startsWith("select")) 
 		{
@@ -36,12 +36,7 @@ public class SakilaTest0046 extends MySQLTest
 	
 	private static Result generatedSQL( Connection conn )
 	{
-		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
-
-Result result = create.select( ((Field)Actor.ACTOR.FIRST_NAME), ((Field)Actor.ACTOR.LAST_NAME), DSL.count(  ) )
-	.from( Actor.ACTOR )
-	.groupBy( ((Field)Actor.ACTOR.FIRST_NAME), ((Field)Actor.ACTOR.LAST_NAME) )
-	.having( DSL.count(  ).greaterThan( DSL.inline( 1 ) ) ).fetch( );
+		
 
 		return result;
 	}
