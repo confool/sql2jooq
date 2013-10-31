47ca638159e7082ec4e2a913a51241b3c4781421
Lukas Eder <lukas.eder@gmail.com>
2013-10-31 11:41:31 +0100

    [#45] Fixed invalid test cases

--- sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest0134.java
+++ sql2jooq-test/test/gudusoft/sql2jooq/sakila/tests/SakilaTest0134.java
@@ -24,7 +24,7 @@ public class SakilaTest0134 extends MySQLTest
 	@Test
 	public void test() throws Exception 
 	{
-		String sql = "select truncate(1.1)";
+		String sql = "select truncate(1.1, 0)";
 		
 		if (sql.toLowerCase().startsWith("select")) 
 		{
@@ -40,7 +40,7 @@ public class SakilaTest0134 extends MySQLTest
 	{
 		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
 
-Result<Record1<Double>> result = create.select( DSL.trunc( DSL.inline( 1.1 ) ) ).fetch( );
+Result<Record1<Double>> result = create.select( DSL.trunc( DSL.inline( 1.1 ), DSL.inline( 0 ) ) ).fetch( );
 
 		return result;
 	}
